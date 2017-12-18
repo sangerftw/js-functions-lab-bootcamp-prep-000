c75e7c186f4c5ca9ad3aa54616b9d33f4f936136
Sara <sangerftw@gmail.com>
2017-12-18 22:15:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+function happyHolidayTo(holiday, name) {
+  return `${holiday}, ${name}!`
+}
